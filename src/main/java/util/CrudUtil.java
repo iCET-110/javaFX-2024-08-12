728219d0b4609c68a9595c7090490548f2f4d646
icet-sharadha <icet.sharadha@gmail.com>
2024-09-04 13:09:27 +0530

    Crud Util

--- src/main/java/util/CrudUtil.java
+++ src/main/java/util/CrudUtil.java
@@ -10,16 +10,16 @@ import java.util.ArrayList;
 public class CrudUtil {
     public static <T> T execute(String SQL, Object... val) throws SQLException {
 
- //   new ArrayList<T>();  ==  <T>T
+        //   new ArrayList<T>();  ==  <T>T
 
         PreparedStatement psTm = DBConnection.getInstance().getConnection().prepareStatement(SQL);
-
+        for (int i = 0; i < val.length; i++) {
+            psTm.setObject(i + 1, val[i]);
+        }
         if (SQL.startsWith("SELECT") || SQL.startsWith("select")) {
             return (T) psTm.executeQuery();
         } else {
-            for (int i = 0; i < val.length; i++) {
-                psTm.setObject(i + 1, val[i]);
-            }
+
             return (T) (Boolean) (psTm.executeUpdate() > 0);
         }
     }

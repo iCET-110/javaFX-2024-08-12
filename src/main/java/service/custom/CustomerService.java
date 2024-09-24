c278e2cbd833cfc46d2b7e3b3fdc9964538db85b
icet-sharadha <icet.sharadha@gmail.com>
2024-09-24 12:02:31 +0530

    Repository Layer

--- src/main/java/service/custom/CustomerService.java
+++ src/main/java/service/custom/CustomerService.java
@@ -1,8 +1,14 @@
 package service.custom;
 
+import javafx.collections.ObservableList;
 import model.Customer;
 import service.SuperService;
 
 public interface CustomerService extends SuperService {
     boolean addCustomer(Customer customer);
+    boolean deleteCustomer(String id);
+    ObservableList<Customer> getAll();
+    boolean updateCustomer(Customer customer);
+    Customer searchCustomer(String id);
+    ObservableList<String> getCustomerIds();
 }
--- src/main/java/service/custom/impl/CustomerServiceImpl.java
+++ src/main/java/service/custom/impl/CustomerServiceImpl.java
@@ -1,5 +1,6 @@
 package service.custom.impl;
 
+import javafx.collections.ObservableList;
 import model.Customer;
 import service.custom.CustomerService;
 
@@ -8,5 +9,30 @@ public class CustomerServiceImpl implements CustomerService {
     public boolean addCustomer(Customer customer) {
         System.out.println("Service Layer : "+customer);
         return false;
+    }
+
+    @Override
+    public boolean deleteCustomer(String id) {
+        return false;
+    }
+
+    @Override
+    public ObservableList<Customer> getAll() {
+        return null;
+    }
+
+    @Override
+    public boolean updateCustomer(Customer customer) {
+        return false;
+    }
+
+    @Override
+    public Customer searchCustomer(String id) {
+        return null;
+    }
+
+    @Override
+    public ObservableList<String> getCustomerIds() {
+        return null;
     }
 }
